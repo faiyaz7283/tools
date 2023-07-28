180a60249b6ef267f5f318cd9a0f2bebb79d96bf
Faiyaz Haider <faiyaz7283@gmail.com>
2023-07-28 15:11:13 -0400

    Updates

--- _next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/join-split":["static\u002Fchunks\u002Fpages\u002Fjoin-split.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fjoin-split"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
